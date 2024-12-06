59e80882d1f0c716435cafd7c99df614ea74df4c
baileyfcross <bf02cros@siena.edu>
2024-12-06 18:23:29 -0500

    first commit

---
+++ backend/src/main/java/com/xavendy/palace/PalaceApplication.java
@@ -0,0 +1,13 @@
+package com.xavendy.palace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalaceApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/xavendy/palace/PalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavendy.palace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
